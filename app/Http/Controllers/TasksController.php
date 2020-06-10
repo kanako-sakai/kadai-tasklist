0844f31fa0e807c58608819b305803de0bb3465b
kanako-sakai <knksk.526@gmail.com>
2020-06-10 03:54:42 +0000

    kadai-tasklist

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task; 
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Get task list
+        $tasks = Task::all();
+        
+        //Show task list
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $task = new Task;
+        
+        //Show task creation view
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //create a task
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        //Redirect to top page
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //get task
+        $task = Task::findOrFail($id);
+        
+        //show on detail view
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //get task
+        $task = Task::findOrFail($id);
+        
+        //show on edit view
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //get task
+        $task = Task::findOrFail($id);
+        
+        //update task
+        $task->content = $request->content;
+        $task->save();
+        
+        //redirect to top page
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //get taks
+        $task = Task::findOrFail($id);
+        
+        //delete task
+        $task->delete();
+        
+        //redirect to top page
+        return redirect('/');
+    }
+}
